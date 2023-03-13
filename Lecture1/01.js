f665d13cc644e66d410c40a0f2cdc85723b678a6
Maharjan Suraj <58384413+maharjansuraj@users.noreply.github.com>
2023-03-13 15:52:42 +0900

    Add files via upload

---
+++ Lecture1/01.js
@@ -0,0 +1,17 @@
+	var a;
+	var b;
+	a = 3;
+	b = 4;
+	var c;
+	c = a+b;
+	alert(c);
+	//document.write("The result is ");
+	//document.write(c);
+	
+	document.write("<h1>Hello</h1>");
+	document.write("I am a student.");
+	
+	var str;
+	str = "Nice to meet you.";
+	document.write(str);
+	alert(str);
---
+++ Lecture1/js/02.js
@@ -0,0 +1,10 @@
+var x = 200;
+var y = 30;
+var z = 300;
+
+var t1 = x*y;
+var t2 = t1 + z;
+
+document.write("subtotal = $" +t1);
+document.write("<br>shipping = $" + z);
+document.write("<br>total = $"+t2);
